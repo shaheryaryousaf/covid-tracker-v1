ae0bfbc827ec111db9d9d0a7e1d9fbaf9be92839
shaheryaryousaf <shaheryary20@gmail.com>
2020-08-29 16:22:16 +0500

    Update Chart.js

--- src/components/MidCards/Chart.js
+++ src/components/MidCards/Chart.js
@@ -1,12 +1,19 @@
-import React, { useEffect, useContext } from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, {
+    useEffect,
+    useContext
+} from 'react';
+import {
+    Bar
+} from 'react-chartjs-2';
 import covidContext from "../../context/covidContext";
 import Loader from "../layout/Loader";
 
 import Paper from '@material-ui/core/Paper';
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import { makeStyles } from '@material-ui/core/styles';
+import {
+    makeStyles
+} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -21,7 +28,11 @@ const Chart = () => {
     const classes = useStyles();
 
     const context = useContext(covidContext);
-    const { stats, fetchStats, loading } = context;
+    const {
+        stats,
+        fetchStats,
+        loading
+    } = context;
 
     useEffect(() => {
         fetchStats();
@@ -29,12 +40,11 @@ const Chart = () => {
     }, []);
 
     if (loading) {
-        return <Loader />
+        return <Loader / >
     }
     const data = {
         labels: [stats.name],
-        datasets: [
-            {
+        datasets: [{
                 label: 'Total Cases',
                 backgroundColor: '#808080',
                 borderColor: '#808080',
@@ -42,6 +52,15 @@ const Chart = () => {
                 hoverBackgroundColor: 'r#808080',
                 hoverBorderColor: '#808080',
                 data: [stats.total]
+            },
+            {
+                label: 'Recovered',
+                backgroundColor: '#FF7F0E',
+                borderColor: '#FF7F0E',
+                borderWidth: 1,
+                hoverBackgroundColor: '#FF7F0E',
+                hoverBorderColor: '#FF7F0E',
+                data: [stats.recovered]
             },
             {
                 label: 'Active Cases',
@@ -61,26 +80,28 @@ const Chart = () => {
                 hoverBorderColor: '#CD2057',
                 data: [stats.deaths]
             },
-            {
-                label: 'Recovered',
-                backgroundColor: '#FF7F0E',
-                borderColor: '#FF7F0E',
-                borderWidth: 1,
-                hoverBackgroundColor: '#FF7F0E',
-                hoverBorderColor: '#FF7F0E',
-                data: [stats.recovered]
-            },
         ]
     };
 
-    return (
-        <Paper className={classes.paper}>
-            <Card variant="outlined" className={classes.card}>
-                <CardContent>
-                    <Bar data={data} />
-                </CardContent>
-            </Card>
-        </Paper>
+    return ( <
+        Paper className = {
+            classes.paper
+        } >
+        <
+        Card variant = "outlined"
+        className = {
+            classes.card
+        } >
+        <
+        CardContent >
+        <
+        Bar data = {
+            data
+        }
+        /> <
+        /CardContent> <
+        /Card> <
+        /Paper>
     );
 }
 
